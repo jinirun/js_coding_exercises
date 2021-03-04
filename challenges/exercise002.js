8874bec202486bf8cc51799800a0c3fbb5cb3d12
Ranjini <ranj.jini@gmail.com>
2021-03-04 21:18:10 +0000

    Implemented Exercise002

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,26 +1,46 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+  return sandwich.fillings;
+
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  if (person.city == "Manchester")
+    return true;
+  return false;
 }
 
 function getBusNumbers(people) {
+  let noOfBuses;
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  noOfBuses = people / 40;
+
+  if(noOfBuses <= 1)
+    return 1;
+  else if (noOfBuses > 1 && noOfBuses <= 2)
+    return 2;
+  else if (noOfBuses > 2 && noOfBuses <= 3)
+    return 3;
+  else
+    return Math.ceil(noOfBuses);
 }
 
 function countSheep(arr) {
+  let count = 0;
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  for(let i in arr){
+    if(arr[i] == "sheep")
+      count += 1;
+  }
+  return count;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  if(person.address.city == "Manchester" && person.address.postCode.startsWith('M'))
+    return true;
+  return false;
 }
 
 module.exports = {

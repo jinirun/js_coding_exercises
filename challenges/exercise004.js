4d747439544d2211558cf69f92217d862ac5b436
Ranjini <ranj.jini@gmail.com>
2021-03-18 23:44:36 +0000

    Refactored exercise004.js - Changed 'for loops' to incorporate map() and filter() in all functions

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,93 +1,43 @@
 function findSmallNums(nums) {
-  let newNums = [];
-  let k = 0;
   if (!nums) throw new Error("nums is required");
-  for(let i=0; i<nums.length; i++){
-    if (nums[i] < 1) {
-      newNums[k] = nums[i];
-      k++;
-    }
-  }
-   return newNums;
+  return nums.filter(nums => nums < 1);
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  let newArray = [];
-  let s = 0;
-  for(let i in names){
-    if (names[i].startsWith(char)){
-      newArray[s] = names[i];
-      s++;
-    }
-  }
-  return newArray;
+  return names.filter(name => name.startsWith(char));
 }
 
 function findVerbs(words) {
-  let returnVerbs = [];
-  let k = 0;
   if (!words) throw new Error("words is required");
-  for(let i in words){
-    if (words[i].startsWith('to ')) {
-      returnVerbs[k] = words[i];
-      k++;
-    }
-  }
-  return returnVerbs;
+  return words.filter(verbs => verbs.startsWith('to '));
 }
 
 function getIntegers(nums) {
-  let k = 0;
-  let returnInt = [];
   if (!nums) throw new Error("nums is required");
-  for(let i in nums){
-    if(nums[i] % 1 == 0) {
-      returnInt[k] = nums[i];
-      k++;
-    }
-  }
-  return returnInt;
+  return nums.filter(returnInt => returnInt % 1 === 0);
 }
 
 function getCities(users) {
-  let returnCity = [];
   if (!users) throw new Error("users is required");
-  for(let i in users){
-    returnCity[i] = users[i].data.city.displayName;
-  }
-  return returnCity;
+  return users.map(user => user.data.city.displayName);
 }
 
 function getSquareRoots(nums) {
-
   if (!nums) throw new Error("nums is required");
-  return ((nums.map(function (x){
-    x = Math.sqrt(x);
-    return +x.toFixed(2);
-  })));
+  return nums.map(x => +(Math.sqrt(x)).toFixed(2));
 }
 
 function findSentencesContaining(sentences, str) {
-  let returnSentences = [];
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-  for(let i in sentences){
-    if ((sentences[i].toLowerCase()).includes(str.toLowerCase())){
-      returnSentences.push(sentences[i]);
-    }
-  }
-  return (returnSentences);
+  return sentences.filter(sentence => sentence.toLowerCase().includes(str.toLowerCase()));
 }
 
 function getLongestSides(triangles) {
-  let largestSide = [];
   if (!triangles) throw new Error("triangles is required");
-  for(let i in triangles){
-    largestSide[i] = Math.max(...triangles[i]);
-  }
-  return largestSide;
+  return triangles.map(sides => Math.max(...sides));
 }
 
 module.exports = {
